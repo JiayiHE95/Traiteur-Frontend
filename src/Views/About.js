9d6711233a8ea36765374a43fd47572d47a698b4
Jiayi HE <he_jia_yi1995@126.com>
2023-05-25 15:40:14 +0200

    responsive design

--- src/Views/About.js
+++ src/Views/About.js
@@ -43,26 +43,25 @@ const About = () => {
   <div className="site-container">
    <NavBar/>
    <div className="site-content">
-   <div className="presentation">
-     <div>Bienvenue sur notre site dédié au traiteur chinois !</div>
-     <div>Nous sommes une équipe de passionnés de cuisine chinoise et nous avons pour mission de vous offrir des plats authentiques et savoureux qui sauront satisfaire toutes vos papilles.
-     </div>
-     <div>Nous utilisons des ingrédients frais et de qualité pour préparer nos plats, en nous inspirant des recettes traditionnelles de la cuisine chinoise tout en y apportant notre touche personnelle.
-     </div>
-     <div>Que ce soit pour un repas en famille, une soirée entre amis ou un événement professionnel, notre service traiteur chinois saura répondre à toutes vos attentes.
-     </div>
-     <div> N'hésitez pas à parcourir notre menu et à passer votre commande en ligne pour une expérience culinaire unique !
-     </div>
+    <div className="presentation">
+      <div>Bienvenue sur notre site dédié au traiteur chinois !</div>
+      <div>Nous sommes une équipe de passionnés de cuisine chinoise et nous avons pour mission de vous offrir des plats authentiques et savoureux qui sauront satisfaire toutes vos papilles.
+      </div>
+      <div>Nous utilisons des ingrédients frais et de qualité pour préparer nos plats, en nous inspirant des recettes traditionnelles de la cuisine chinoise tout en y apportant notre touche personnelle.
+      </div>
+      <div>Que ce soit pour un repas en famille, une soirée entre amis ou un événement professionnel, notre service traiteur chinois saura répondre à toutes vos attentes.
+      </div>
+      <div> N'hésitez pas à parcourir notre menu et à passer votre commande en ligne pour une expérience culinaire unique !
+      </div>
     </div>
     <div className="slideshow-container">
-     <div className="slideshow">
-        <div className="prev" onClick={() => slideIndex!==1? plusSlides(-1):setSlideIndex(slides.length)}>&#10094;</div>
-        {slides}
-        <div className="next" onClick={() => slideIndex===slides.length? setSlideIndex(1):plusSlides(1)}>&#10095;</div>
-     </div>
-     <div className="dot-container">{dots}</div>
+      <div className="slideshow">
+          <div className="prev" onClick={() => slideIndex!==1? plusSlides(-1):setSlideIndex(slides.length)}>&#10094;</div>
+          {slides}
+          <div className="next" onClick={() => slideIndex===slides.length? setSlideIndex(1):plusSlides(1)}>&#10095;</div>
+      </div>
+      <div className="dot-container">{dots}</div>
     </div>
-    
    </div>
   </div>
  )
